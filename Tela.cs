f348467cfa15e1cade859a2710c1e09c3703b2c5
Vinicius Floriano <viniciushpfloriano@outlook.com>
2022-06-20 22:06:41 -0300

    Implementando restrições de movimentos

--- Tela.cs
+++ Tela.cs
@@ -73,6 +73,7 @@ namespace xadrez_console
         public static PosicaoXadrez lerPosicaoXadrez()
         {
             string s = Console.ReadLine();
+            
             char coluna = s[0];
             int linha = int.Parse(s[1] + "");
             return new PosicaoXadrez(coluna, linha);
